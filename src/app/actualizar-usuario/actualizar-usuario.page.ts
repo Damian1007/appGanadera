353ae430d3d95e26a645ea0afcb56ca9e135f568
Damian Molina <damian.molina@correounivalle.edu.co>
2023-04-23 14:19:52 -0500

    Ultimos Ajustes 2

--- src/app/actualizar-usuario/actualizar-usuario.page.ts
+++ src/app/actualizar-usuario/actualizar-usuario.page.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Subscription, map } from 'rxjs';
 import { Usuario } from '../interfaces/usuario';
 import { AutentificarService } from '../services/autentificar.service';
+import { IonModal } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-actualizar-usuario',
@@ -11,19 +14,32 @@ import { AutentificarService } from '../services/autentificar.service';
   styleUrls: ['./actualizar-usuario.page.scss'],
 })
 export class ActualizarUsuarioPage implements OnInit {
-
+  @ViewChild(IonModal) modal: IonModal;
+  
   usuario : Usuario;
   usuarioId : any = localStorage.getItem("usuarioId");
   usuarioSub : Subscription;
+  dptoSub : Subscription;
+  citySub : Subscription;
+  isSubmitted = false;
+
+  isModalOpen = false;
+  isModalOpen2 = false;
+  departamentos : any[];
+  departamentosAux : any[];
+  dpto : any;
+  dptoCodigo : any;
+  ciudades : any[];
+  ciudadesAux : any[];
+  ciud : any;
 
   form = this.formBuilder.group({
-    correo: ['', [Validators.email, Validators.required]],
+    correo: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
     nombre: ['', [Validators.required]],
     apellido: ['', [Validators.required]],
-    contrasena: ['', [Validators.required]],
-    confirmarContrasena: ['', [Validators.required]],
+    contrasena: [''],
     pais: ['', [Validators.required]],
-    telefono: ['', [Validators.required]],
+    telefono: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
     departamento: ['', [Validators.required]],
     ciudad: ['', [Validators.required]],
   });
@@ -32,6 +48,7 @@ export class ActualizarUsuarioPage implements OnInit {
     private formBuilder : FormBuilder,
     private autentificarService : AutentificarService,
     private router : Router,
+    private http : HttpClient
   ) { 
     this.usuario = {
       correo: '',
@@ -46,41 +63,139 @@ export class ActualizarUsuarioPage implements OnInit {
    }
 
   ngOnInit() {
-    this.usuarioSub = this.autentificarService.getUsuario(this.usuarioId).subscribe(usu => {
+    this.usuarioSub = this.autentificarService.getUsuario(this.usuarioId).subscribe(usuario => {
       this.form.setValue({
-        correo: usu.correo,
-        nombre: usu.nombre,
-        apellido: usu.apellido,
-        contrasena: '',
-        confirmarContrasena: '',
-        pais: usu.pais,
-        telefono: usu.telefono,
-        departamento: usu.departamento,
-        ciudad: usu.ciudad,
+        correo: usuario.correo,
+        nombre: usuario.nombre,
+        apellido: usuario.apellido,
+        contrasena: usuario.contrasena,
+        pais: usuario.pais,
+        telefono: usuario.telefono,
+        departamento: usuario.departamento,
+        ciudad: usuario.ciudad,
       });
+    });
+
+    this.dptoSub = this.getDpto().subscribe(dptos => {
+      this.departamentos = dptos;
+      this.departamentosAux = dptos;
+    });
+
+    this.citySub = this.getCity().subscribe(ciudades => {
+      this.ciudades = ciudades;
+      this.ciudadesAux = ciudades;
     });
   }
 
   ionViewDidLeave() {
     this.usuarioSub.unsubscribe();
+    this.citySub.unsubscribe();
+    this.dptoSub.unsubscribe();
+  }
+
+  // --------------------------------------------- SearchBars ------------------------------------------------
+  buscarDpto(ev : any) {
+    const text = ev.target.value;
+    
+    if(text && text.trim() != '') {
+      this.departamentosAux = this.departamentosAux.filter((depto : any) => {
+        return (depto.depto.toLowerCase().indexOf(text.toLowerCase()) > -1);
+      })
+    }else{
+      this.departamentosAux = this.departamentos;
+    }
+  }
+
+  buscarCiudad(ev : any) {
+    const text = ev.target.value;
+    
+    if(text && text.trim() != '') {
+      this.ciudadesAux = this.ciudadesAux.filter((ciudad : any) => {
+        return (ciudad.Ciudad.toLowerCase().indexOf(text.toLowerCase()) > -1);
+      })
+    }else{
+      this.ciudadesAux = this.ciudades;
+    }
+  }
+
+  // ---------------------------------- Cambios y Consultas Departamento y Ciudad -------------------------------------------------
+  getDpto() {
+    return this.http.get("assets/archivos/departamentos.json").pipe( map((res:any) => {
+      return res.data;
+    }));
+  }
+
+  getCity() {
+    return this.http.get("assets/archivos/ciudades.json").pipe( map((res:any) => {
+      return res.data;
+    }));
+  }
+
+  dptoChange(e : any) {
+    this.dpto = e.depto;
+    this.dptoCodigo = e.codigo;
+    this.ciudadesAux = this.ciudades;
+
+    this.ciudadesAux = this.ciudadesAux.filter((ciudades : any) => {
+      return (ciudades.Depto == this.dptoCodigo);
+    })
+    this.ciud = '';
+
+    this.setOpen(false, 1);
+  }
+
+  ciudadChange(e : any) {
+    this.ciud = e.Ciudad;
+    //console.log(this.ciud['Depto']);
+    this.setOpen(false, 2);
+  }
+  // -----------------------------------------------------------------------------------------------------------------
+
+  setOpen(isOpen : boolean, num : any) {
+    if(num == 1) {
+      this.isModalOpen = isOpen;
+    }
+    if(num == 2) {
+      this.isModalOpen2 = isOpen;
+    }
   }
 
   actualizarUsuario() {
-    this.usuario.correo = this.form.getRawValue().correo;
-    this.usuario.nombre = this.form.getRawValue().nombre;
-    this.usuario.apellido = this.form.getRawValue().apellido;
-    this.usuario.contrasena = this.form.getRawValue().contrasena;
-    this.usuario.pais = this.form.getRawValue().pais;
-    this.usuario.telefono = this.form.getRawValue().telefono;
-    this.usuario.departamento = this.form.getRawValue().departamento;
-    this.usuario.ciudad = this.form.getRawValue().ciudad;
+    this.isSubmitted = true;
 
-    this.autentificarService.updateUsuario(this.usuario, this.usuarioId)
-    .then(() => {
-      this.router.navigate(['/usuario'], { replaceUrl: true });
-    })
-    .catch(error => {
-      console.log('Error al Actualizar usuario', error);
-    });
+    if(this.form.valid) {
+      this.usuario.correo = this.form.getRawValue().correo;
+      this.usuario.nombre = this.form.getRawValue().nombre;
+      this.usuario.apellido = this.form.getRawValue().apellido;
+      this.usuario.contrasena = this.form.getRawValue().contrasena;
+      this.usuario.pais = this.form.getRawValue().pais;
+      this.usuario.telefono = this.form.getRawValue().telefono;
+      this.usuario.departamento = this.form.getRawValue().departamento;
+      this.usuario.ciudad = this.form.getRawValue().ciudad;
+
+      this.autentificarService.updateUsuario(this.usuario, this.usuarioId)
+      .then(() => {
+        this.router.navigate(['/usuario'], { replaceUrl: true });
+      })
+      .catch(error => {
+        console.log('Error al Actualizar usuario', error);
+      });
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+
+  get errorControl() {
+    return this.form.controls;
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+
+    if (ev.detail.role === 'backdrop') {
+      //console.log(this.evento);
+      this.setOpen(false, 1);
+      this.setOpen(false, 2);
+    }
   }
 }
